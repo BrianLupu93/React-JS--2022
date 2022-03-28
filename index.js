d8a861c023765db344a33a654313a2d8cee97a1b
BrianLupu93 <brianlupu18@gmail.com>
2022-03-28 21:41:19 +0200

    imperative vs declarative example

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-ReactDOM.render(
-	<ul>
-		<li>Fanta</li>
-		<li>Coca Cola</li>
-		<li>Sprite</li>
-	</ul>,
-	document.getElementById('root')
-);
+//  DECLARATIVE REACT JS
+
+ReactDOM.render(<h1 className="header">This is an imperative way to program</h1>, document.getElementById('root'));
+
+//  IMPERATIVE ex: VANILLA JS
+
+// const h1 = document.createElement('h1');
+// h1.textContent = 'This is an imperative way to program';
+// h1.className = 'header';
+// document.getElementById('root').append(h1);
