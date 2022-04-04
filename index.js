8a66e048fa10ddf1ad316c27b43e06325cf0fd9a
BrianLupu93 <brianlupu18@gmail.com>
2022-04-04 11:29:00 +0200

    to point 10

---
+++ index.js
@@ -0,0 +1,13 @@
+const navBar = (
+	<nav>
+		<h1>This is my website name</h1>
+		<ul>
+			<li>Menu</li>
+			<li>About</li>
+			<li>Contact</li>
+		</ul>
+	</nav>
+);
+
+// JSX stand for JavaScrpit XTML
+ReactDOM.render(navBar, document.getElementById('root'));
