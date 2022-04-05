734dd7fc28d8f049029a80bad241d01aeafc9563
BrianLupu93 <brianlupu18@gmail.com>
2022-04-05 11:44:25 +0200

    App.js setState and Shallow Merge

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
 		super();
 
 		this.state = {
-			name: 'Brian'
+			name: { firstName: 'Brian', lastName: 'Lupu' },
+			company: 'ZTM'
 		};
 	}
 
@@ -17,10 +18,13 @@ class App extends Component {
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
-					<p>Hi {this.state.name}</p>
+					<p>
+						Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
+					</p>
 					<button
 						onClick={() => {
-							this.setState({ name: 'Andrei' });
+							this.setState({ name: { firstName: 'Andrei', lastName: 'Pula' } });
+							console.log(this.state);
 						}}
 					>
 						Change Name
