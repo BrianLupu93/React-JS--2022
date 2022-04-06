471124100e1952f863012027ef0592e2b435ef4f
BrianLupu93 <brianlupu18@gmail.com>
2022-04-06 06:41:23 +0200

    App.js search bar

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -10,9 +10,11 @@ class App extends Component {
 		this.state = {
 			monsters: []
 		};
+		console.log('1. constructor');
 	}
 
 	componentDidMount() {
+		console.log('3. componentDidMount');
 		fetch('https://jsonplaceholder.typicode.com/users').then((response) => response.json()).then((users) =>
 			this.setState(
 				() => {
@@ -26,8 +28,17 @@ class App extends Component {
 	}
 
 	render() {
+		console.log('2. render');
 		return (
 			<div className="App">
+				<input
+					className="search-box"
+					type="search"
+					placeholder="search monsters"
+					onChange={(event) => {
+						console.log(event.target.value);
+					}}
+				/>
 				{this.state.monsters.map((monster) => {
 					return <h1 key={monster.id}>{monster.name}</h1>;
 				})}
