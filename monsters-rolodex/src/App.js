051ed72859dcf1e173824649d11974f8aa1b7f86
BrianLupu93 <brianlupu18@gmail.com>
2022-04-06 09:41:50 +0200

    import search box

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 
 import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-bar.component';
 import logo from './logo.svg';
 import './App.css';
 
@@ -40,10 +41,7 @@ class App extends Component {
 
 		return (
 			<div className="App">
-				<input className="search-box" type="search" placeholder="search monsters" onChange={onSearchChange} />
-				{/*filteredMonsters.map((monster) => {
-					return <h1 key={monster.id}>{monster.name}</h1>;
-				})*/}
+				<SearchBox className="search-box" onChangeHandler={onSearchChange} placeholder="search monsters" />
 				<CardList monsters={filteredMonsters} />
 			</div>
 		);
