978bece8ded5aa5b1031fc089247d2643dfb2b22
BrianLupu93 <brianlupu18@gmail.com>
2022-04-08 10:23:39 +0200

    test request

--- test1-example/src/App.js
+++ test1-example/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [ country, setCountry ] = useState([ 'romania' ]);
+
+	useEffect(() => {
+		async function universityRequest() {
+			const response = await fetch(`http://universities.hipolabs.com/search?country=${country}`);
+			const data = await response.json();
+			console.log(data);
+		}
+		universityRequest();
+	}, []);
+
+	return (
+		<div>
+			<h1>Search University</h1>
+		</div>
+	);
+};
 
 export default App;
