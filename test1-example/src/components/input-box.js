d8fc553883eec867bc63af5e8577f9e9c58aa8a8
BrianLupu93 <brianlupu18@gmail.com>
2022-04-11 10:13:09 +0200

    input-box.js functionality

---
+++ test1-example/src/components/input-box.js
@@ -0,0 +1,5 @@
+const InputBox = ({ className, type, placeholder, value, onChange }) => {
+	return <input className={className} type={type} placeholder={placeholder} value={value} onChange={onChange} />;
+};
+
+export default InputBox;
