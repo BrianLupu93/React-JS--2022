3f51372629f2ca799f5422cfd584f2b01679d578
BrianLupu93 <brianlupu18@gmail.com>
2022-04-11 07:51:18 +0200

    App.js fetch input country

--- test1-example/src/App.js
+++ test1-example/src/App.js
@@ -2,20 +2,38 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
-	const [ country, setCountry ] = useState([ 'romania' ]);
+	const [ inputCountry, setInputCountry ] = useState('');
+	const [ country, setCountry ] = useState('');
+	const [ universities, setUniversities ] = useState([]);
+	const [ test, setTest ] = useState('');
+
+	const countryNameImputed = (event) => {
+		const inputString = event.target.value;
+		setInputCountry(inputString);
+	};
+
+	const submitCountryName = (event) => {
+		setCountry(inputCountry);
+	};
+
+	console.log(country);
 
 	useEffect(() => {
-		async function universityRequest() {
-			const response = await fetch(`http://universities.hipolabs.com/search?country=${country}`);
-			const data = await response.json();
-			console.log(data);
-		}
-		universityRequest();
+		fetch(`http://universities.hipolabs.com/search?country=${country}`)
+			.then((response) => response.json())
+			.then((unv) => setUniversities({ unv }));
 	}, []);
+
+	console.log(universities);
 
 	return (
 		<div>
 			<h1>Search University</h1>
+			<p>Type the country</p>
+			<input className="countryInputString" type="text" placeholder="ex: Italy" onChange={countryNameImputed} />
+			<button className="submitButton" type="submit" onClick={submitCountryName}>
+				OK
+			</button>
 		</div>
 	);
 };
