9877982098024efc0a65e7966568579d4804a494
BrianLupu93 <brianlupu18@gmail.com>
2022-04-12 08:09:45 +0200

    links

--- Capstone-Project/crwn-clothing/src/App.js
+++ Capstone-Project/crwn-clothing/src/App.js
@@ -1,23 +1,14 @@
 import { Routes, Route, Outlet } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
-
-const Navigation = () => {
-	return (
-		<div>
-			<div>
-				<h1>I am the navigation bar</h1>
-			</div>
-			<Outlet />
-		</div>
-	);
-};
+import Navigation from './routes/navigations/navigation.component';
 
 const App = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Navigation />}>
 				<Route index element={<Home />} />
+				<Route index element={<Shop />} />
 			</Route>
 		</Routes>
 	);
