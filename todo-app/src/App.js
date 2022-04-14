08162159168909e989848ecbec47fa8947c8a001
BrianLupu93 <brianlupu18@gmail.com>
2022-04-14 10:21:13 +0200

    reset all App

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,5 @@
-function App() {
-	return (
-		<div>
-			<h1>To-Do-App</h1>
-			<h3>powered by Brian</h3>
-		</div>
-	);
-}
+const App = () => {
+	return <div>TO-DO-APP</div>;
+};
 
 export default App;
