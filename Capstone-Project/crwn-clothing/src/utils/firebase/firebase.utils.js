a9989c0062c9bdd2ceca9dd4d2379bcfbea4f55a
BrianLupu93 <brianlupu18@gmail.com>
2022-04-14 06:38:36 +0200

    added out sign is component  as well as firebase  utils  and brought in firebase libraries

--- Capstone-Project/crwn-clothing/src/utils/firebase/firebase.utils.js
+++ Capstone-Project/crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -1,6 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
 const firebaseConfig = {
 	apiKey: 'AIzaSyCfds-3vmAwf5iQl5mKjv1EPsWodWWlYuk',
 	authDomain: 'crwn-clothing-db-c7751.firebaseapp.com',
@@ -10,7 +12,6 @@ const firebaseConfig = {
 	appId: '1:246088599627:web:61904675491aba94a325ce'
 };
 
-// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
 const provider = new GoogleAuthProvider();
@@ -19,5 +20,31 @@ provider.setCustomParameters({
 });
 
 export const auth = getAuth();
-
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+	const userDocRef = doc(db, 'users', userAuth.uid);
+
+	const userSnapshot = await getDoc(userDocRef);
+
+	if (!userSnapshot.exists()) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await setDoc(userDocRef, { displayName, email, createdAt });
+		} catch (error) {
+			console.log('error creating the user', error.message);
+		}
+	}
+
+	return userDocRef;
+
+	//  if user data does not exist
+	// create / set the document with the data from userAuth in my collection
+
+	// if user data exist
+	// return userDocRef
+};
