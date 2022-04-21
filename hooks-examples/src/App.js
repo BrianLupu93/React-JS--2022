662b6dba8998c35d6dca5e24b70ae91797b6d649
BrianLupu93 <brianlupu18@gmail.com>
2022-04-21 07:19:02 +0200

    counter App

--- hooks-examples/src/App.js
+++ hooks-examples/src/App.js
@@ -1,5 +1,27 @@
+import { useState } from 'react';
+
+import './App.css';
+
 const App = () => {
-	return;
+	const [ counter, setCounter ] = useState(0);
+
+	const decrementCounter = () => {
+		console.log('decrement');
+		setCounter((prevCounter) => prevCounter - 1);
+	};
+
+	const incrementCounter = () => {
+		console.log('increment');
+		setCounter((prevCounter) => prevCounter + 1);
+	};
+
+	return (
+		<div>
+			<button onClick={decrementCounter}>-</button>
+			<h3>{counter}</h3>
+			<button onClick={incrementCounter}>+</button>
+		</div>
+	);
 };
 
 export default App;
