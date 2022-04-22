19331a398ebbcd2d5da9376ba1cbab600513f7c5
BrianLupu93 <brianlupu18@gmail.com>
2022-04-22 07:29:47 +0200

    useMeme Hooks

--- hooks-examples/src/App.js
+++ hooks-examples/src/App.js
@@ -1,18 +1,47 @@
-import { useState, createContext } from 'react';
-
-import User from './components/user.component';
-import Login from './components/Login.component';
-
-export const AppContext = createContext(null);
+import axios from 'axios';
+import { useEffect, useState, useMemo } from 'react';
 
 const App = () => {
-	const [ username, setUsername ] = useState('');
+	const [ data, setData ] = useState(null);
+	const [ toggle, setToggle ] = useState(false);
+
+	useEffect(() => {
+		axios.get('https://jsonplaceholder.typicode.com/comments').then((response) => {
+			setData(response.data);
+		});
+	}, []);
+
+	const findLongestName = (comments) => {
+		if (!comments) return null;
+
+		let longestName = '';
+		for (let i = 0; i < comments.length; i++) {
+			let currentName = comments[i].name;
+			if (currentName.length > longestName.length) {
+				longestName = currentName;
+			}
+		}
+		console.log('THIS WAS COMPUTED');
+
+		return longestName;
+	};
+
+	const getLongestName = useMemo(() => findLongestName(data), [ data ]);
 
 	return (
-		<AppContext.Provider value={{ username, setUsername }}>
-			<Login />
-			<User />
-		</AppContext.Provider>
+		<div className="App">
+			<div>{getLongestName}</div>
+
+			<button
+				onClick={() => {
+					setToggle(!toggle);
+				}}
+			>
+				{''}
+				Toggle
+			</button>
+			{toggle && <h1>toggle</h1>}
+		</div>
 	);
 };
 
