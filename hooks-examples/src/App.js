4ae843dbcca1901c6d53df29d550e2868f76875c
BrianLupu93 <brianlupu18@gmail.com>
2022-04-26 06:56:54 +0200

    create react app

--- hooks-examples/src/App.js
+++ hooks-examples/src/App.js
@@ -1,47 +1,11 @@
-import axios from 'axios';
-import { useEffect, useState, useMemo } from 'react';
+import './App.css';
+import { NameList } from './components/NameList';
 
 const App = () => {
-	const [ data, setData ] = useState(null);
-	const [ toggle, setToggle ] = useState(false);
-
-	useEffect(() => {
-		axios.get('https://jsonplaceholder.typicode.com/comments').then((response) => {
-			setData(response.data);
-		});
-	}, []);
-
-	const findLongestName = (comments) => {
-		if (!comments) return null;
-
-		let longestName = '';
-		for (let i = 0; i < comments.length; i++) {
-			let currentName = comments[i].name;
-			if (currentName.length > longestName.length) {
-				longestName = currentName;
-			}
-		}
-		console.log('THIS WAS COMPUTED');
-
-		return longestName;
-	};
-
-	const getLongestName = useMemo(() => findLongestName(data), [ data ]);
-
 	return (
-		<div className="App">
-			<div>{getLongestName}</div>
-
-			<button
-				onClick={() => {
-					setToggle(!toggle);
-				}}
-			>
-				{''}
-				Toggle
-			</button>
-			{toggle && <h1>toggle</h1>}
-			<h1>Brian</h1>
+		<div>
+			<h1>This is the main App Container</h1>
+			<NameList />
 		</div>
 	);
 };
