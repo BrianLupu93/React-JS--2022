12dd5375fde6fd0849acb8e7ddaf4109ab286d12
BrianLupu93 <brianlupu18@gmail.com>
2022-05-19 10:58:22 +0300

    feature and chart added

--- purple-test/src/components/featured/Featured.jsx
+++ purple-test/src/components/featured/Featured.jsx
@@ -1,5 +1,9 @@
 import './featured.scss';
 import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import { KeyboardArrowDown } from '@mui/icons-material';
+import { KeyboardArrowUp } from '@mui/icons-material';
 
 export const Featured = () => {
 	return (
@@ -11,7 +15,33 @@ export const Featured = () => {
 
 			<div className="bottom">
 				<div className="featuredChart">
-					<div>chart</div>
+					<CircularProgressbar value={70} text={'70%'} strokeWidth={5} />
+				</div>
+				<p className="title">Total sales made today</p>
+				<p className="amount">$420</p>
+				<p className="desc">Previous transactions processing. Last pay,ents may not be included.</p>
+				<div className="summary">
+					<div className="item">
+						<div className="itemTitle">Target</div>
+						<div className="itemResult negative">
+							<KeyboardArrowDown fontSize="small" />
+							<div className="resultAmount">$12.4k</div>
+						</div>
+					</div>
+					<div className="item">
+						<div className="itemTitle">Last Week</div>
+						<div className="itemResult positive">
+							<KeyboardArrowUp fontSize="small" />
+							<div className="resultAmount">$12.4k</div>
+						</div>
+					</div>
+					<div className="item">
+						<div className="itemTitle">Last Month</div>
+						<div className="itemResult negative">
+							<KeyboardArrowDown fontSize="small" />
+							<div className="resultAmount ">$12.4k</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
